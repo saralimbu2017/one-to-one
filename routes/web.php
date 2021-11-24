7073673f4d42057a0f28de27e37ff355d993e1a6
saralimbu2017 <iamsebi7@gmail.com>
2021-11-25 00:09:00 +1100

    Adding descriptive comments to routes defined in web.php

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Route for insert operation
 Route::get('/insert',function(){
     $user = User::findOrFail(1);
     $address = new Address(['name'=>'123 New address']);
     $user->address()->save($address);
 });
 
+//Route for update operation
 Route::get('/update',function(){
     $address = Address::whereUserId(1)->first();
     $address->name = 'Updated Mel';
     $address->save();
 });
 
+//Route for read operation
 Route::get('/read',function(){
     $user = User::findOrFail(1);
     echo $user->address->name;
 });
 
+//Route for delete operation
 Route::get('/delete',function(){
     $user = User::findOrFail(1);
     $user->address()->delete();
